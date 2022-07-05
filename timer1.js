2d217210159026503f144ec61ea4a778b3103ae3
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-05 22:41:24 +0000

    initial commit of timer1.js

---
+++ timer1.js
@@ -0,0 +1,20 @@
+const myArgs = process.argv.slice(2);
+const times = myArgs
+  .filter(x => x >= 0)
+  .filter(x => !isNaN(x))
+  .map(x => x * 1000)
+  .sort((a, b) => {return a - b});
+
+  //Test console
+  console.log(times)
+
+
+for (const time of times) {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+    process.stdout.write(`.\n`);
+  }, time);
+}
+
+
+process.stdout.write('\x07');
