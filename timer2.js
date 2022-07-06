46c67548ba32038ee5ad95c3d0d178f5b416b771
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-06 03:35:54 +0000

    initial commmit of timer2.js

---
+++ timer2.js
@@ -0,0 +1,25 @@
+const stdin = process.stdin;
+const stdout = process.stdout;
+stdin.setEncoding('utf8');
+stdin.setRawMode(true);
+
+const beep = () => {
+  return stdout.write(`.\n`);
+};
+
+stdin.on('data', data => {
+  if (data === 'b') {
+    beep();
+  }
+  if (data >= 1 && data <= 9 ) {
+    timer = data * 1000;
+    stdout.write(`Setting timer for ${data} seconds...\n`);
+    setTimeout(() => {
+      stdout.write(`.\n`);
+    }, timer);
+  }
+  if (data === '\u0003') {
+    process.stdout.write('Thanks for using me, ciao!\n');
+    process.exit();
+  }
+});
